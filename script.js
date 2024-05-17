1f1415e37a9c2ba400a25ab5f9f2b254b6eca4fe
Renatas <mazeikaa18@gmail.com>
2024-05-17 21:08:13 +0300

    implemented dark mode button and its functionality

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const toggleMenu = document.querySelector('.navbar-nav');
+const lightMode = document.querySelector('.light-mode');
+const darkMode = document.querySelector('.dark-mode');
+const bcg = document.querySelector('#background');
+const whiteFontElements = document.querySelectorAll('.white-font'); // all elements with text has this class
 
-toggleMenu.addEventListener('click', function () {});
+darkMode.addEventListener('click', function () {
+  bcg.style.backgroundColor = '#001523';
+  darkMode.style.color = 'white';
+  whiteFontElements.forEach(element => {
+    element.style.color = 'white';
+  });
+  lightMode.style.color = 'white';
+  darkMode.classList.toggle('hidden');
+  lightMode.classList.toggle('hidden');
+});
+
+lightMode.addEventListener('click', function () {
+  bcg.style.backgroundColor = '#fffae6';
+  darkMode.style.color = 'black';
+  whiteFontElements.forEach(element => {
+    element.style.color = 'black';
+  });
+  lightMode.classList.toggle('hidden');
+  darkMode.classList.toggle('hidden');
+});
